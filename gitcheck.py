f925675394fd0e9bae4364d2d5e14f3cd8676c51
badele <bruno.adele@jesuislibre.org>
2013-04-24 16:26:42 +0200

    First version with check remote status

--- gitcheck.py
+++ gitcheck.py
@@ -64,6 +64,7 @@ def checkRepository(rep, verbose=False):
     acount = len(aitem)
     mcount = len(mitem)
     dcount = len(ditem)
+    tcount = acount + mcount + dcount
     change = not (acount == 0 and mcount == 0 and dcount == 0)
     if not change:
         color = tcolor.DEFAULT + tcolor.GREEN
@@ -74,27 +75,49 @@ def checkRepository(rep, verbose=False):
     prjname = "%s%s%s" % (color, rep, tcolor.DEFAULT)
     if change:
         countstr = "%sLocal%s[" % (tcolor.ORANGE, tcolor.DEFAULT)
-        if mcount > 0:
-            countstr += "%sModifify:%s%s" % (
+        if tcount > 0:
+            countstr += "%sTo commit:%s%s" % (
                 tcolor.BLUE,
                 tcolor.DEFAULT,
-                mcount
+                getLocalNbChange(rep)
             )
-        if acount > 0:
-            countstr += " / %sAdd:%s%s" % (
-                tcolor.BLUE,
-                tcolor.DEFAULT,
-                acount
-            )
-        if dcount > 0:
-            countstr += " / %sDelete:%s%s" % (
-                tcolor.BLUE,
-                tcolor.DEFAULT,
-                dcount
-            )
+
+        # if mcount > 0:
+        #     countstr += "%sModifify:%s%s" % (
+        #         tcolor.BLUE,
+        #         tcolor.DEFAULT,
+        #         mcount
+        #     )
+        # if acount > 0:
+        #     countstr += " / %sAdd:%s%s" % (
+        #         tcolor.BLUE,
+        #         tcolor.DEFAULT,
+        #         acount
+        #     )
+        # if dcount > 0:
+        #     countstr += " / %sDelete:%s%s" % (
+        #         tcolor.BLUE,
+        #         tcolor.DEFAULT,
+        #         dcount
+        #     )
         countstr += "]"
     else:
         countstr = ""
+
+    branch = getDefaultBranch(rep)
+    if branch != "":
+        remotes = getRemoteRepositories(rep)
+        for r in remotes:
+            count = getRemoteNbChange(rep, r, branch)
+            if count > 0:
+                countstr += " %s%s%s[%sTo Push:%s%s]" % (
+                    tcolor.ORANGE,
+                    r,
+                    tcolor.DEFAULT,
+                    tcolor.BLUE,
+                    tcolor.DEFAULT,
+                    count
+                )
 
     print("%(prjname)s/%(branch)s %(countstr)s" % locals())
 
@@ -110,6 +133,31 @@ def checkRepository(rep, verbose=False):
             print(filename)
 
 
+def getLocalNbChange(rep):
+    curdir = os.path.abspath(os.getcwd())
+    snbchange = re.compile(r'(^[0-9]+)')
+    result = sysexec("cd %(curdir)s/%(rep)s ; git status -suno | wc -l"
+                     % locals())
+
+    m = snbchange.match(result)
+    if m:
+        return m.group(1)
+
+    return 0
+
+
+def getRemoteNbChange(rep, remote, branch):
+    snbchange = re.compile(r'(^[0-9]+)')
+    result = gitExec(rep, "git log %(remote)s/%(branch)s..HEAD --oneline | wc -l"
+                     % locals())
+
+    m = snbchange.match(result)
+    if m:
+        return int(m.group(1))
+
+    return 0
+
+
 # Get Default branch for repository
 def getDefaultBranch(rep):
     curdir = os.path.abspath(os.getcwd())
@@ -123,6 +171,29 @@ def getDefaultBranch(rep):
         branch = m.group(1)
 
     return branch
+
+
+def getRemoteRepositories(rep):
+    remotes = []
+    result = gitExec(rep, "git remote"
+                     % locals())
+
+    lines = result.split('\n')
+    for l in lines:
+        if l != "":
+            remotes.append(l)
+
+    return remotes
+
+
+# Custom git command
+def gitExec(rep, command):
+    curdir = os.path.abspath(os.getcwd())
+    cmd = "cd %(curdir)s/%(rep)s ; %(command)s" % locals()
+
+    result = sysexec(cmd)
+
+    return result
 
 
 # Check all git repositories
