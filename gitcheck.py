d79f2ab40d37677a55fbcdd23ad4c92b346dd6bc
Marcin Radziwoński <m.radziwonski@epro.com.pl>
2014-02-21 09:05:23 +0100

    Added maxdepth and quiet options

--- gitcheck.py
+++ gitcheck.py
@@ -21,27 +21,30 @@ class tcolor:
     BELL = "\a"
 
 # Search all local repositories from current directory
-def searchRepositories(dir=None):
+def searchRepositories(dir=None, depth=None):
 
     # if trailing slash is in argument of option -d, it won't work -> get rid of it
     if dir != None and dir[-1:] == '/':
         dir = dir[:-1]
     curdir = os.path.abspath(os.getcwd()) if dir is None else dir
+    startinglevel = curdir.count(os.sep)        
     repo = []
     rsearch = re.compile(r'^/?(.*?)/\.git')
     for root, dirnames, filenames in os.walk(curdir):
-        for dirnames in fnmatch.filter(dirnames, '*.git'):
-            fdir = os.path.join(root, dirnames)
-            fdir = fdir.replace(curdir, '')
-            m = rsearch.match(fdir)
-            if m:
-                repo.append(os.path.join(curdir, m.group(1)))
+        level = root.count(os.sep) - startinglevel                
+        if depth == None or level <= depth:            
+            for dirnames in fnmatch.filter(dirnames, '*.git'):
+                fdir = os.path.join(root, dirnames)
+                fdir = fdir.replace(curdir, '')
+                m = rsearch.match(fdir)
+                if m:
+                    repo.append(os.path.join(curdir, m.group(1)))
 
     return repo
 
 
 # Check state of a git repository
-def checkRepository(rep, verbose=False, ignoreBranch=r'^$'):
+def checkRepository(rep, verbose=False, ignoreBranch=r'^$', quiet=False):
     aitem = []
     mitem = []
     ditem = []
@@ -87,86 +90,86 @@ def checkRepository(rep, verbose=False, ignoreBranch=r'^$'):
                     tcolor.DEFAULT,
                     count
                 )
+    if ischange or not quiet:            
+        if ischange:
+            color = tcolor.BOLD + tcolor.RED
+        else:
+            color = tcolor.DEFAULT + tcolor.GREEN
 
-    if ischange:
-        color = tcolor.BOLD + tcolor.RED
-    else:
-        color = tcolor.DEFAULT + tcolor.GREEN
+        # Remove trailing slash from repository/directory name
+        if rep[-1:] == '/':
+            rep = rep[:-1]
 
-    # Remove trailing slash from repository/directory name
-    if rep[-1:] == '/':
-        rep = rep[:-1]
+        # Do some magic to not show the absolute path as repository name
+        # Case 1: script was started in a directory that is a git repo
+        if rep == os.path.abspath(os.getcwd()):
+            (head, tail) = os.path.split(rep)
+            if tail != '':
+                repname = tail
+        # Case 2: script was started in a directory with possible subdirs that contain git repos
+        elif rep.find(os.path.abspath(os.getcwd())) == 0:
+            repname = rep[len(os.path.abspath(os.getcwd()))+1:]
+        # Case 3: script was started with -d and above cases do not apply
+        else:
+            repname = rep
 
-    # Do some magic to not show the absolute path as repository name
-    # Case 1: script was started in a directory that is a git repo
-    if rep == os.path.abspath(os.getcwd()):
-        (head, tail) = os.path.split(rep)
-        if tail != '':
-            repname = tail
-    # Case 2: script was started in a directory with possible subdirs that contain git repos
-    elif rep.find(os.path.abspath(os.getcwd())) == 0:
-        repname = rep[len(os.path.abspath(os.getcwd()))+1:]
-    # Case 3: script was started with -d and above cases do not apply
-    else:
-        repname = rep
+        # Print result
+        prjname = "%s%s%s" % (color, repname, tcolor.DEFAULT)
+        if len(changes) > 0:
+            strlocal = "%sLocal%s[" % (tcolor.ORANGE, tcolor.DEFAULT)
+            strlocal += "%sTo Commit:%s%s" % (
+                tcolor.BLUE,
+                tcolor.DEFAULT,
+                len(getLocalFilesChange(rep))
+            )
 
-    # Print result
-    prjname = "%s%s%s" % (color, repname, tcolor.DEFAULT)
-    if len(changes) > 0:
-        strlocal = "%sLocal%s[" % (tcolor.ORANGE, tcolor.DEFAULT)
-        strlocal += "%sTo Commit:%s%s" % (
-            tcolor.BLUE,
-            tcolor.DEFAULT,
-            len(getLocalFilesChange(rep))
-        )
+            strlocal += "]"
+        else:
+            strlocal = ""
 
-        strlocal += "]"
-    else:
-        strlocal = ""
-
-    print("%(prjname)s/%(branch)s %(strlocal)s%(topush)s%(topull)s" % locals())
-    if verbose:
-        if ischange > 0:
-            filename = "  |--Local"
-            print(filename)
-            for c in changes:
-                filename = "     |--%s%s%s" % (
-                    tcolor.ORANGE,
-                    c[1],
-                    tcolor.DEFAULT)
+        print("%(prjname)s/%(branch)s %(strlocal)s%(topush)s%(topull)s" % locals())
+        if verbose:
+            if ischange > 0:
+                filename = "  |--Local"
                 print(filename)
+                for c in changes:
+                    filename = "     |--%s%s%s" % (
+                        tcolor.ORANGE,
+                        c[1],
+                        tcolor.DEFAULT)
+                    print(filename)
 
-        if branch != "":
-            remotes = getRemoteRepositories(rep)
-            for r in remotes:
-                commits = getLocalToPush(rep, r, branch)
-                if len(commits) > 0:
-                    rname = "  |--%(r)s" % locals()
-                    print(rname)
-                    for commit in commits:
-                        commit = "     |--%s[To Push]%s %s%s%s" % (
-                            tcolor.MAGENTA,
-                            tcolor.DEFAULT,
-                            tcolor.BLUE,
-                            commit,
-                            tcolor.DEFAULT)
-                        print(commit)
+            if branch != "":
+                remotes = getRemoteRepositories(rep)
+                for r in remotes:
+                    commits = getLocalToPush(rep, r, branch)
+                    if len(commits) > 0:
+                        rname = "  |--%(r)s" % locals()
+                        print(rname)
+                        for commit in commits:
+                            commit = "     |--%s[To Push]%s %s%s%s" % (
+                                tcolor.MAGENTA,
+                                tcolor.DEFAULT,
+                                tcolor.BLUE,
+                                commit,
+                                tcolor.DEFAULT)
+                            print(commit)
 
-        if branch != "":
-            remotes = getRemoteRepositories(rep)
-            for r in remotes:
-                commits = getRemoteToPull(rep, r, branch)
-                if len(commits) > 0:
-                    rname = "  |--%(r)s" % locals()
-                    print(rname)
-                    for commit in commits:
-                        commit = "     |--%s[To Pull]%s %s%s%s" % (
-                            tcolor.MAGENTA,
-                            tcolor.DEFAULT,
-                            tcolor.BLUE,
-                            commit,
-                            tcolor.DEFAULT)
-                        print(commit)
+            if branch != "":
+                remotes = getRemoteRepositories(rep)
+                for r in remotes:
+                    commits = getRemoteToPull(rep, r, branch)
+                    if len(commits) > 0:
+                        rname = "  |--%(r)s" % locals()
+                        print(rname)
+                        for commit in commits:
+                            commit = "     |--%s[To Pull]%s %s%s%s" % (
+                                tcolor.MAGENTA,
+                                tcolor.DEFAULT,
+                                tcolor.BLUE,
+                                commit,
+                                tcolor.DEFAULT)
+                            print(commit)
 
     return actionNeeded
 
@@ -244,8 +247,8 @@ def gitExec(rep, command):
 
 
 # Check all git repositories
-def gitcheck(verbose, checkremote, ignoreBranch, bellOnActionNeeded, shouldClear, searchDir):
-    repo = searchRepositories(searchDir)
+def gitcheck(verbose, checkremote, ignoreBranch, bellOnActionNeeded, shouldClear, searchDir, depth, quiet):    
+    repo = searchRepositories(searchDir, depth)
     actionNeeded = False
 
     if checkremote:
@@ -257,7 +260,7 @@ def gitcheck(verbose, checkremote, ignoreBranch, bellOnActionNeeded, shouldClear
         print(tcolor.RESET)
 
     for r in repo:
-        if checkRepository(r, verbose, ignoreBranch):
+        if checkRepository(r, verbose, ignoreBranch, quiet):
             actionNeeded = True
 
     if actionNeeded and bellOnActionNeeded:
@@ -268,24 +271,27 @@ def usage():
     print("Usage: %s [OPTIONS]" % (sys.argv[0]))
     print("Check multiple git repository in one pass")
     print("== Common options ==")
-    print("  -v, --verbose                 Show files & commits")
-    print("  -r, --remote                  force remote update(slow)")
-    print("  -b, --bell                    bell on action needed")
-    print("  -w <sec>, --watch <sec>       after displaying, wait <sec> and run again")
-    print("  -i <re>, --ignore-branch <re> ignore branches matching the regex <re>")
-    print("  -d <dir>,                     Search <dir> for repositories")
+    print("  -v, --verbose                        Show files & commits")
+    print("  -r, --remote                         force remote update(slow)")
+    print("  -b, --bell                           bell on action needed")
+    print("  -w <sec>, --watch=<sec>              after displaying, wait <sec> and run again")
+    print("  -i <re>, --ignore-branch=<re>        ignore branches matching the regex <re>")
+    print("  -d <dir>, --dir=<dir>                Search <dir> for repositories")
+    print("  -m <maxdepth>, --maxdepth=<maxdepth> Limit the depth of repositories search")
+    print("  -q, --quiet                          Display info only when repository needs action")
 
-
-def main():
+def main():    
     try:
         opts, args = getopt.getopt(
             sys.argv[1:],
-            "vhrbw:i:d:",
-            ["verbose", "help", "remote", "bell", "watch:", "ignore-branch:",
-             "dir:"])
+            "vhrbw:i:d:m:q",
+            ["verbose", "help", "remote", "bell", "watch=", "ignore-branch=",
+             "dir=", "maxdepth=", "quiet"])
     except getopt.GetoptError, e:
         if e.opt == 'w' and 'requires argument' in e.msg:
             print "Please indicate nb seconds for refresh ex: gitcheck -w10"
+        else:    
+            print e.msg
         sys.exit(2)
 
     verbose = False
@@ -293,26 +299,42 @@ def main():
     watchInterval = 0
     bellOnActionNeeded = False
     searchDir = None
+    depth = None
+    quiet = False
     ignoreBranch = r'^$'  # empty string
-    for opt, arg in opts:
+    for opt, arg in opts:        
         if opt in ("-v", "--verbose"):
             verbose = True
-        if opt in ("-r", "--remote"):
+        elif opt in ("-r", "--remote"):
             checkremote = True
-        if opt in ("-r", "--remote"):
+        elif opt in ("-r", "--remote"):
             checkremote = True
-        if opt in ("-b", "--bell"):
+        elif opt in ("-b", "--bell"):
             bellOnActionNeeded = True
-        if opt in ("-w", "--watch"):
-            watchInterval = arg
-        if opt in ("-i", "--ignore-branch"):
+        elif opt in ("-w", "--watch"):
+            try:
+                watchInterval = float(arg)
+            except ValueError:
+                print "option %s requires numeric value" % opt
+                sys.exit(2)              
+        elif opt in ("-i", "--ignore-branch"):
             ignoreBranch = arg
-        if opt in ("-d", "--dir"):
+        elif opt in ("-d", "--dir"):
             searchDir = arg
-            
-        if opt in ("-h", "--help"):
+        elif opt in ("-m", '--maxdepth'):
+            try:
+                depth = int(arg)
+            except ValueError:
+                print "option %s requires int value" % opt
+                sys.exit(2)  
+        elif opt in ("-q", "--quiet"):
+            quiet = True    
+        elif opt in ("-h", "--help"):
             usage()
             sys.exit(0)
+#        else:            
+#            print "Unhandled option %s" % opt
+#            sys.exit(2)
 
     while True:
         gitcheck(
@@ -321,12 +343,17 @@ def main():
             ignoreBranch,
             bellOnActionNeeded,
             watchInterval > 0,
-            searchDir
+            searchDir,
+            depth,
+            quiet
         )
         if watchInterval:
-            time.sleep(float(watchInterval))
+            time.sleep(watchInterval)
         else:
             break
 
 if __name__ == "__main__":
-    main()
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.exit(0)
