cb52ffb334a8c04b0380d9261043d20af7d01108
Christian Tremblay, ing <christian.tremblay@servisys.com>
2014-10-22 20:46:40 -0400

    Modification des couleurs Version fonctionnelle sous Windows
    Aucune gestion des caractères accentués par contre
    
    Details suppl.:
    
    Signed-off-by: Christian Tremblay, ing. <christian.tremblay@servisys.com>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ Remove unsupported github tags:
 
 setup(
     name='gitcheck',
-    version='0.3.8',
+    version='0.3.9',
     description='Check multiple git repository in one pass, gitExec modified by Christian Tremblay',
     long_description=rst('README.rst'),
 
